7fbda57cba40823585bd776e98a9ff6053de75f8
romagny13 <romagny13@yahoo.fr>
2020-06-21 09:52:03 +0200

    Castle INotifyPropertyChanged mixin sample

---
+++ CastleNotifyPropertyChangedMixinSample/ViewModelLocator.cs
@@ -0,0 +1,71 @@
+﻿using Castle.DynamicProxy;
+using CastleNotifyPropertyChangedMixinSample.ViewModels;
+using System;
+using System.ComponentModel;
+
+namespace CastleNotifyPropertyChangedMixinSample
+{
+    public class ViewModelLocator
+    {
+        private ProxyGenerator generator;
+        private MainWindowViewModel mainWindowViewModel;
+
+        public ViewModelLocator()
+        {
+            generator = new ProxyGenerator(new PersistentProxyBuilder());
+
+            Initialize();
+
+            generator.ProxyBuilder.ModuleScope.SaveAssembly();
+        }
+
+        private void Initialize()
+        {
+            // MainWindowViewModel
+            var options = new ProxyGenerationOptions();
+            options.AddMixinInstance(new PropertyChangedNotifier());
+            mainWindowViewModel = generator.CreateClassProxy<MainWindowViewModel>(options, new PropertyChangedInterceptor());
+        }
+
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get { return mainWindowViewModel; }
+        }
+    }
+
+    public interface IPropertyChangedNotifier : INotifyPropertyChanged
+    {
+        void OnPropertyChanged(object target, string propertyName);
+    }
+
+    [Serializable]
+    public class PropertyChangedNotifier : IPropertyChangedNotifier
+    {
+        public void OnPropertyChanged(object target, string propertyName)
+        {
+            PropertyChanged?.Invoke(target, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class PropertyChangedInterceptor : IInterceptor
+    {
+        private const string SetConstant = "set_";
+
+        public void Intercept(IInvocation invocation)
+        {
+            invocation.Proceed();
+
+            if (invocation.Method.Name.StartsWith(SetConstant))
+            {
+                var notifier = invocation.Proxy as IPropertyChangedNotifier;
+                if (notifier != null)
+                {
+                    string propertyName = invocation.Method.Name.Substring(SetConstant.Length);
+                    notifier.OnPropertyChanged(invocation.Proxy, propertyName);
+                }
+            }
+        }
+    }
+}
---
+++ CastleNotifyPropertyChangedMixinSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Commands;
+
+namespace CastleNotifyPropertyChangedMixinSample.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        private DelegateCommand updateTitleCommand;
+
+        public MainWindowViewModel()
+        {
+            Title = "Main Window";
+        }
+
+        public virtual string Title { get; set; }
+
+        public DelegateCommand UpdateTitleCommand
+        {
+            get
+            {
+                if (updateTitleCommand == null)
+                    updateTitleCommand = new DelegateCommand(ExecuteUpdateTitleCommand);
+                return updateTitleCommand;
+            }
+        }
+
+        private void ExecuteUpdateTitleCommand()
+        {
+            Title += "!";
+        }
+    }
+}
