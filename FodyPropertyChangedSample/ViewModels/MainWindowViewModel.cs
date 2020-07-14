8f81c15222858b46676f192c0439edf8afc0d0bb
romagny13 <romagny13@yahoo.fr>
2020-07-14 13:09:33 +0200

    Fody PropertyChanged Sample

---
+++ FodyPropertyChangedSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Commands;
+using PropertyChanged;
+using System.Threading.Tasks;
+
+namespace FodyPropertyChangedSample.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MainWindowViewModel
+    {
+        private DelegateCommand updateTitleCommand;
+
+        public MainWindowViewModel()
+        {
+            Title = "The title";
+        }
+
+        public string Title { get; set; }
+        public bool IsBusy { get; set; }
+
+        public DelegateCommand UpdateTitleCommand
+        {
+            get
+            {
+                if (updateTitleCommand == null)
+                    updateTitleCommand = new DelegateCommand(ExecuteUpdateTitleCommand);
+                return updateTitleCommand;
+            }
+        }
+
+        private void ExecuteUpdateTitleCommand()
+        {
+            IsBusy = true;
+
+            Task.Delay(3000).Await(() =>
+            {
+                Title += "!";
+                IsBusy = false;
+            });
+        }
+    }
+}
